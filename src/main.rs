d2b9cb63e81fd47e51c48ec8d66e99db87af5325
Andrey Avsenin <snatvb@ya.ru>
2023-07-01 21:23:47 +0300

    Added basic impl fsm

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,14 @@
 extern crate dotenv;
-use std::env;
-use teloxide::{prelude::*, utils::command::BotCommands};
+pub mod state;
+use std::{env, error::Error};
+use teloxide::{
+    prelude::*,
+    types::{
+        InlineKeyboardButton, InlineKeyboardMarkup, InlineQueryResultArticle, InputMessageContent,
+        InputMessageContentText, Me,
+    },
+    utils::command::BotCommands,
+};
 // #[macro_use]
 // extern crate dotenv_codegen;
 
@@ -12,17 +20,8 @@ use teloxide::{prelude::*, utils::command::BotCommands};
 enum Command {
     #[command(description = "display this text.")]
     Help,
-}
-
-async fn answer(bot: Bot, msg: Message, cmd: Command) -> ResponseResult<()> {
-    match cmd {
-        Command::Help => {
-            bot.send_message(msg.chat.id, Command::descriptions().to_string())
-                .await?
-        }
-    };
-
-    Ok(())
+    #[command(description = "Start")]
+    Start,
 }
 
 #[tokio::main]
@@ -37,7 +36,109 @@ async fn main() {
 
     let bot = Bot::new(teloxide_token);
 
-    Command::repl(bot, answer).await;
+    let handler = dptree::entry()
+        .branch(Update::filter_message().endpoint(message_handler))
+        .branch(Update::filter_callback_query().endpoint(callback_handler))
+        .branch(Update::filter_inline_query().endpoint(inline_query_handler));
+
+    Dispatcher::builder(bot, handler)
+        .enable_ctrlc_handler()
+        .build()
+        .dispatch()
+        .await;
+}
+
+async fn inline_query_handler(
+    bot: Bot,
+    q: InlineQuery,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    let choose_debian_version = InlineQueryResultArticle::new(
+        "0",
+        "Chose debian version",
+        InputMessageContent::Text(InputMessageContentText::new("Debian versions:")),
+    )
+    .reply_markup(make_keyboard());
+
+    bot.answer_inline_query(q.id, vec![choose_debian_version.into()])
+        .await?;
+
+    Ok(())
+}
+
+/// When it receives a callback from a button it edits the message with all
+/// those buttons writing a text with the selected Debian version.
+///
+/// **IMPORTANT**: do not send privacy-sensitive data this way!!!
+/// Anyone can read data stored in the callback button.
+async fn callback_handler(bot: Bot, q: CallbackQuery) -> Result<(), Box<dyn Error + Send + Sync>> {
+    if let Some(version) = q.data {
+        let text = format!("You chose: {version}");
+
+        // Tell telegram that we've seen this query, to remove 🕑 icons from the
+        // clients. You could also use `answer_callback_query`'s optional
+        // parameters to tweak what happens on the client side.
+        bot.answer_callback_query(q.id).await?;
+
+        // Edit text of the message to which the buttons were attached
+        if let Some(Message { id, chat, .. }) = q.message {
+            bot.edit_message_text(chat.id, id, text).await?;
+        } else if let Some(id) = q.inline_message_id {
+            bot.edit_message_text_inline(id, text).await?;
+        }
+
+        log::info!("You chose: {}", version);
+    }
+
+    Ok(())
+}
+
+async fn message_handler(
+    bot: Bot,
+    msg: Message,
+    me: Me,
+) -> Result<(), Box<dyn Error + Send + Sync>> {
+    if let Some(text) = msg.text() {
+        match BotCommands::parse(text, me.username()) {
+            Ok(Command::Help) => {
+                // Just send the description of all commands.
+                bot.send_message(msg.chat.id, Command::descriptions().to_string())
+                    .await?;
+            }
+            Ok(Command::Start) => {
+                // Create a list of buttons and send them.
+                let keyboard = make_keyboard();
+                bot.send_message(msg.chat.id, "Debian versions:")
+                    .reply_markup(keyboard)
+                    .await?;
+            }
+
+            Err(_) => {
+                bot.send_message(msg.chat.id, "Command not found!").await?;
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn make_keyboard() -> InlineKeyboardMarkup {
+    let mut keyboard: Vec<Vec<InlineKeyboardButton>> = vec![];
+
+    let debian_versions = [
+        "Buzz", "Rex", "Bo", "Hamm", "Slink", "Potato", "Woody", "Sarge", "Etch", "Lenny",
+        "Squeeze", "Wheezy", "Jessie", "Stretch", "Buster", "Bullseye",
+    ];
+
+    for versions in debian_versions.chunks(3) {
+        let row = versions
+            .iter()
+            .map(|&version| InlineKeyboardButton::callback(version.to_owned(), version.to_owned()))
+            .collect();
+
+        keyboard.push(row);
+    }
+
+    InlineKeyboardMarkup::new(keyboard)
 }
 
 fn load_env() {
---
+++ src/state.rs
@@ -0,0 +1,97 @@
+use async_trait::async_trait;
+use std::error::Error;
+
+use teloxide::{
+    requests::Requester,
+    types::{CallbackQuery, ChatId, InlineQuery, Message},
+};
+
+pub struct FSM {
+    pub state: Box<dyn State>,
+    pub context: Context,
+}
+
+impl FSM {
+    pub fn new(state: Box<dyn State>, context: Context) -> Self {
+        Self { state, context }
+    }
+
+    pub async fn handle_message(&mut self, msg: Message) {
+        let handled = self.state.handle_message(&self.context.clone(), msg).await;
+        self.state = match handled {
+            Ok(state) => state,
+            Err(error) => self.handle_failure(error).await,
+        };
+    }
+
+    pub async fn handle_callback_query(&mut self, callback_query: CallbackQuery) {
+        let handled = self
+            .state
+            .handle_callback_query(self.context.clone(), callback_query)
+            .await;
+        self.state = match handled {
+            Ok(state) => state,
+            Err(error) => self.handle_failure(error).await,
+        };
+    }
+
+    pub async fn handle_inline_query(&mut self, inline_query: InlineQuery) {
+        let handled = self
+            .state
+            .handle_inline_query(self.context.clone(), inline_query)
+            .await;
+        self.state = match handled {
+            Ok(state) => state,
+            Err(error) => self.handle_failure(error).await,
+        };
+    }
+
+    async fn handle_failure(&mut self, error: Box<dyn Error + Send + Sync>) -> Box<dyn State> {
+        log::error!("Error handling message: {}", error);
+        let _ = self
+            .context
+            .bot
+            .send_message(self.context.chat_id, "Something went wrong...")
+            .await;
+        self.state.clone()
+    }
+}
+
+#[derive(Clone)]
+pub struct Context {
+    pub bot: teloxide::Bot,
+    pub chat_id: ChatId,
+}
+
+impl Context {
+    pub fn new(bot: teloxide::Bot, chat_id: ChatId) -> Self {
+        Self { bot, chat_id }
+    }
+}
+
+type StateOutput = Result<Box<dyn State>, Box<dyn Error + Send + Sync>>;
+
+#[async_trait]
+pub trait State: Send + Sync {
+    async fn on_enter(&self) {}
+
+    async fn handle_message(&mut self, _: &Context, _: Message) -> StateOutput {
+        Ok(self.clone_state())
+    }
+
+    async fn handle_callback_query(&self, _: Context, _: CallbackQuery) -> StateOutput {
+        Ok(self.clone_state())
+    }
+
+    async fn handle_inline_query(&self, _: Context, _: InlineQuery) -> StateOutput {
+        Ok(self.clone_state())
+    }
+
+    fn clone_state(&self) -> Box<dyn State>;
+}
+
+impl Clone for Box<dyn State> {
+    fn clone(&self) -> Box<dyn State> {
+        self.clone_state()
+    }
+}
